38d8f3351c2b6cb9b03ffea9accd01dbfe1d95df
PC-Gamer <jrg_compras@hotmail.com>
2018-12-08 19:02:21 -0200

    Base de dados json para popular banco.

---
+++ ToDoList.Repositories/Data/BaseJSON.js
@@ -0,0 +1,134 @@
+//Estado Civil (MaritalStatus)
+[
+   {
+      "status": "Solteiro(a)"
+   },
+   {
+      "status": "Casado(a)"
+   },
+   {
+      "status": "Divorciado(a)"
+   },
+   {
+      "status": "Viuvo(a)"
+   },
+   {
+      "status": "Enrolado(a)"
+   },
+]
+
+//Parentescos
+[
+   {
+      "status": "Filho(a)"
+   },
+   {
+      "status": "Primo(a)"
+   },
+   {
+      "status": "Pai"
+   },
+   {
+      "status": "Mae"
+   },
+   {
+      "status": "Tio(a)"
+   }
+]
+
+//Associado (Associated)
+[
+   {
+      "name": "Sarah Shelton",
+      "cpf": 9515153905,
+      "dateJoin": "12/12/2018",
+      "adress": "364 River Street, 4209",
+      "city": "Macdona",
+      "uf": "Texas",
+      "cep": "88970-000",
+      "email": "sarahshelton@comvex.com",
+      "birthDate": "16/09/1996",
+      "maritalStatus": "1"
+   },
+   {
+      "name": "Josie Schroeder",
+      "cpf": 9515153905,
+      "dateJoin": "12/12/2018",
+      "adress": "810 Falmouth Street, 8666",
+      "city": "Limestone",
+      "uf": "Colorado",
+      "cep": "88970-000",
+      "email": "josieschroeder@comvex.com",
+      "birthDate": "16/09/1996",
+      "maritalStatus": "2"
+   },
+   {
+      "name": "Byrd Kirby",
+      "cpf": 9515153905,
+      "dateJoin": "12/12/2018",
+      "adress": "243 Tiffany Place, 8892",
+      "city": "Ladera",
+      "uf": "Virginia",
+      "cep": "88970-000",
+      "email": "byrdkirby@comvex.com",
+      "birthDate": "16/09/1996",
+      "maritalStatus": "3"
+   },
+   {
+      "name": "Prince Brewer",
+      "cpf": 9515153905,
+      "dateJoin": "12/12/2018",
+      "adress": "984 Oriental Court, 8231",
+      "city": "Cobbtown",
+      "uf": "New Mexico",
+      "cep": "88970-000",
+      "email": "princebrewer@comvex.com",
+      "birthDate": "16/09/1996",
+      "maritalStatus": "4"
+   },
+   {
+      "name": "Heidi Tucker",
+      "cpf": 9515153905,
+      "dateJoin": "12/12/2018",
+      "adress": "873 Bainbridge Street, 2735",
+      "city": "Ruckersville",
+      "uf": "Northern Mariana Islands",
+      "cep": "88970-000",
+      "email": "heiditucker@comvex.com",
+      "birthDate": "16/09/1996",
+      "maritalStatus": "5"
+   }
+]
+//Dependentes
+[
+   {
+     "name": "Dolores Blackwell",
+     "kinshipid": 1,
+     "birthDate": "16/09/1996",
+     "associatedid": 1
+   },
+   {
+     "name": "Carney Sparks",
+     "kinshipid": 2,
+     "birthDate": "16/09/1996",
+     "associatedid": 2
+   },
+   {
+     "name": "Sue Cruz",
+     "kinshipid": 3,
+     "birthDate": "16/09/1996",
+     "associatedid": 3
+   },
+   {
+     "name": "Norma Black",
+     "kinshipid": 4,
+     "birthDate": "16/09/1996",
+     "associatedid": 4
+   },
+   {
+     "name": "Sanders Gentry",
+     "kinshipid": 5,
+     "birthDate": "16/09/1996",
+     "associatedid": 5
+   }
+ ]
